20e8ed431848e994b5a519fe747df4c88d0ecdbd
Marco Pacchialat <m.pacchialat@gmail.com>
2022-05-06 10:35:16 +0200

    scaffolding

---
+++ Cinema/Controllers/CinemaRoomsController.cs
@@ -0,0 +1,160 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CinemaClient.Data;
+using CinemaClient.Domain;
+
+namespace CinemaClient.Controllers;
+
+public class CinemaRoomsController : Controller
+{
+    private readonly CinemaContext _context;
+
+    public CinemaRoomsController(CinemaContext context)
+    {
+        _context = context;
+    }
+
+    // GET: CinemaRooms
+    public async Task<IActionResult> Index()
+    {
+        var cinemaContext = _context.Rooms.Include(c => c.Movie);
+        return View(await cinemaContext.ToListAsync());
+    }
+
+    // GET: CinemaRooms/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var cinemaRoom = await _context.Rooms
+            .Include(c => c.Movie)
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (cinemaRoom == null)
+        {
+            return NotFound();
+        }
+
+        return View(cinemaRoom);
+    }
+
+    // GET: CinemaRooms/Create
+    public IActionResult Create()
+    {
+        ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id");
+        return View();
+    }
+
+    // POST: CinemaRooms/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Capacity,MovieId")] CinemaRoom cinemaRoom)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(cinemaRoom);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", cinemaRoom.MovieId);
+        return View(cinemaRoom);
+    }
+
+    // GET: CinemaRooms/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var cinemaRoom = await _context.Rooms.FindAsync(id);
+        if (cinemaRoom == null)
+        {
+            return NotFound();
+        }
+        ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", cinemaRoom.MovieId);
+        return View(cinemaRoom);
+    }
+
+    // POST: CinemaRooms/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Capacity,MovieId")] CinemaRoom cinemaRoom)
+    {
+        if (id != cinemaRoom.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(cinemaRoom);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CinemaRoomExists(cinemaRoom.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", cinemaRoom.MovieId);
+        return View(cinemaRoom);
+    }
+
+    // GET: CinemaRooms/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var cinemaRoom = await _context.Rooms
+            .Include(c => c.Movie)
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (cinemaRoom == null)
+        {
+            return NotFound();
+        }
+
+        return View(cinemaRoom);
+    }
+
+    // POST: CinemaRooms/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var cinemaRoom = await _context.Rooms.FindAsync(id);
+        _context.Rooms.Remove(cinemaRoom);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool CinemaRoomExists(int id)
+    {
+        return _context.Rooms.Any(e => e.Id == id);
+    }
+}
---
+++ Cinema/Controllers/SpectatorsController.cs
@@ -0,0 +1,161 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CinemaClient.Data;
+using CinemaClient.Domain;
+
+namespace CinemaClient.Controllers
+{
+    public class SpectatorsController : Controller
+    {
+        private readonly CinemaContext _context;
+
+        public SpectatorsController(CinemaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Spectators
+        public async Task<IActionResult> Index()
+        {
+            var cinemaContext = _context.Spectators.Include(s => s.Ticket);
+            return View(await cinemaContext.ToListAsync());
+        }
+
+        // GET: Spectators/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var spectator = await _context.Spectators
+                .Include(s => s.Ticket)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (spectator == null)
+            {
+                return NotFound();
+            }
+
+            return View(spectator);
+        }
+
+        // GET: Spectators/Create
+        public IActionResult Create()
+        {
+            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Id");
+            return View();
+        }
+
+        // POST: Spectators/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Surname,BirthDate,TicketId")] Spectator spectator)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(spectator);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Id", spectator.TicketId);
+            return View(spectator);
+        }
+
+        // GET: Spectators/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var spectator = await _context.Spectators.FindAsync(id);
+            if (spectator == null)
+            {
+                return NotFound();
+            }
+            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Id", spectator.TicketId);
+            return View(spectator);
+        }
+
+        // POST: Spectators/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Surname,BirthDate,TicketId")] Spectator spectator)
+        {
+            if (id != spectator.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(spectator);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SpectatorExists(spectator.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Id", spectator.TicketId);
+            return View(spectator);
+        }
+
+        // GET: Spectators/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var spectator = await _context.Spectators
+                .Include(s => s.Ticket)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (spectator == null)
+            {
+                return NotFound();
+            }
+
+            return View(spectator);
+        }
+
+        // POST: Spectators/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var spectator = await _context.Spectators.FindAsync(id);
+            _context.Spectators.Remove(spectator);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SpectatorExists(int id)
+        {
+            return _context.Spectators.Any(e => e.Id == id);
+        }
+    }
+}
